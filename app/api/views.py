dbbfd71dc7d8f0d7c2632122e4f451f38c5a4aba
Jez Abot <jez.abot@students.pcci.edu>
2024-10-10 12:43:40 -0500

    serializing user models

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+#User model and serializer imports
+from .models import Missionary, Church
+from .serializer import MissionarySerializer, ChurchSerializer
+
 # Create your views here.
