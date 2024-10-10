4add1461b8f8b0e11f01e1cb6589782bfadac6d8
Jez Abot <jez.abot@students.pcci.edu>
2024-10-10 01:28:43 -0500

    User Models

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # User Models
+class Missionary(models.Model):
+   MissionaryName = models.CharField()
+   MissionaryField = models.CharField()
+
+class Church(models.Model):
+   ChurchName = models.CharField()
+   ChurchLocation = models.IntegerField()
