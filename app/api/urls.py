baeec9764a65619de0ac3324dce524cde0cbcd64
Jez Abot <jez.abot@students.pcci.edu>
2024-10-14 21:38:16 -0500

    chaning pages

--- app/api/urls.py
+++ app/api/urls.py
@@ -2,28 +2,11 @@ from django.urls import path, include
 
 #Imports all views from view.py
 from . import views
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register(r'missionary', views.Missionary_view_set)
-router.register(r'church', views.Church_view_set)
 
 urlpatterns = [
-   #Router URLs
-   path('', include(router.urls)),
-   #path ('api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
    path('missionaries/', views.missionaries, name = 'missionaries'),
    path('churches/', views.churches, name = 'churches'),
    path('users/', views.users, name = 'users'),
 
-   #Missionary List, Create URL
-   path('missionary/',          views.Missionary_LC_view.as_view(),  name = "Missionary-List-Create"),
-   #Missionary Retrieve, Update, Delete URL
-   path('missionary/<int:pk>/', views.Missionary_RUD_view.as_view(), name = "Missionary-Retrieve-Update-Delete"),
-
-   #Church List, Create URL
-   path('church/',          views.Church_LC_view.as_view(),  name = "Church-List-Create"),
-   #Church Retrieve, Update, Delete URL
-   path('church/<int:pk>/', views.Church_RUD_view.as_view(), name = "Church-Retrieve-Update-Delete")
 ]
 
--- app/api/views.py
+++ app/api/views.py
@@ -3,52 +3,20 @@ from django.shortcuts import render
 #My imports 
 from .models                 import Missionary, Church
 from .serializer             import MissionarySerializer, ChurchSerializer
-from rest_framework          import generics, status, mixins, viewsets
 from django.http             import JsonResponse, HttpResponse
 from rest_framework.parsers  import JSONParser
 from rest_framework.response import Response
-from rest_framework.generics import RetrieveUpdateDestroyAPIView
 import requests 
-from rest_framework.decorators import api_view, renderer_classes
-
-#Missionary List/Create Class
-class Missionary_LC_view(generics.ListCreateAPIView):
-   queryset         = Missionary.objects.all()
-   serializer_class = MissionarySerializer
-#Missionary View Set Class
-class Missionary_view_set(viewsets.ModelViewSet):
-   queryset         = Missionary.objects.all().order_by('missionary_name')
-   serializer_class = MissionarySerializer
-#Missionary Retrieve/Update/Destroy Class
-class Missionary_RUD_view(generics.RetrieveUpdateDestroyAPIView):
-   queryset         = Missionary.objects.all()
-   serializer_class = MissionarySerializer
-   lookup_field     = "pk"
-   
-
-#Church List/Create Class
-class Church_LC_view(generics.ListCreateAPIView):
-   queryset         = Church.objects.all()
-   serializer_class = ChurchSerializer
-#Church View Set Class
-class Church_view_set(viewsets.ModelViewSet):
-   queryset         = Church.objects.all().order_by('church_name')
-   serializer_class = ChurchSerializer
-#Church Retrieve/Update/Destroy View Class
-class Church_RUD_view(generics.RetrieveUpdateDestroyAPIView):
-   queryset         = Church.objects.all()
-   serializer_class = ChurchSerializer
-   lookup_field     = "pk"
 
 def missionaries(request):
-   response = requests.get('http://127.0.0.1:8000/missionary/') 
-   data = response.json()
-   return render(request, 'missionaries.html', {'data': data})
+   missionary_data = Missionary.objects.all()
+   context = {'missionaries': missionary_data}
+   return render(request, 'missionaries.html', context)
 
 def churches(request):
-   response = requests.get('http://127.0.0.1:8000/church/')
-   data = response.json()
-   return render(request, 'churches.html', {'data': data})
+   church_data = Church.objects.all()
+   context = {'churches': church_data}
+   return render(request, 'churches.html', context)
 
 def users(request):
    church_data = Church.objects.all()
