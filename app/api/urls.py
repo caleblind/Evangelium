ec5570646166cb206a052934114452083b79bc27
Kraken <cekahnke@gmail.com>
2024-10-18 12:57:07 -0500

    Added templates, views, and urls for sign_up, log_in, and log_out

--- app/api/urls.py
+++ app/api/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
    path('users/', views.users, name = 'users'),
    path('christian/', views.christian, name = 'christian'),
 
-]
+   path('login/', views.login_page, name='login_page'),
+   path('signup/', views.signup_page, name='signup_page'),  # Optional for registration
+   path('connections/', views.connections_list, name='connections_list'),
+   path('logout/', views.logout_view, name='logout'),
 
+]
