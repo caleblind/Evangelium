c72030041efbb63fde122793d7f27c3d078d7b7e
caleblind <57119431+caleblind@users.noreply.github.com>
2024-10-20 16:52:17 -0400

    Remove unused url

--- app/api/urls.py
+++ app/api/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
    path('matching/', views.matching, name='matching'),
+   #path('login/', views.login, name='login'),
    path("signup/", views.authView, name ="authView"),
    path("accounts/", include("django.contrib.auth.urls")),
 ]
