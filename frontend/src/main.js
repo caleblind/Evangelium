5de2835e86570109512bea6d6e77de2cd221838f
niwe <bakakis101@gmail.com>
2024-11-02 18:08:43 -0500

    i just want to switch branches in peace:/

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,5 @@ import App from "./App.vue";
 import router from "./router"; // initialize the router
 
 const app = createApp(App);
-app.mount("#app");
 app.use(router);
+app.mount("#app");
--- frontend/src/pages/LandingPage.vue
+++ frontend/src/pages/LandingPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app">
+  <div class="landingpage">
     <HeroSection />
     <AboutUsSection />
     <BenefitsSection />
