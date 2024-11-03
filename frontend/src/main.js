494eae3089d56ba5a46b8ec30a62d86ae18acd5c
niwe <bakakis101@gmail.com>
2024-11-02 20:47:10 -0500

    fixed white boarder around the website landing page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router"; // initialize the router
+import router from "./router"; // Initialize the router
+import "./assets/styles.css"; // Import global styles
 
 const app = createApp(App);
 app.use(router);
