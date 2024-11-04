4258359c5eb5169f8b61c1ca2674a92035f21ec0
niwe <bakakis101@gmail.com>
2024-11-04 12:18:30 -0600

    SignupPage

---
+++ frontend/src/pages/SignUpPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="signuppage">
+    <!-- Displays progress bar dynamically-->
+    <ProgressBarSection />
+
+    <!-- Displays signup form dynamically-->
+    <FormSection />
+  </div>
+</template>
+
+<script>
+import ProgressBarSection from "@/components/signup/ProgressBarSection.vue";
+import FormSection from "@/components/signup/FormSection.vue";
+
+export default {
+  name: "SignUpPage",
+  components: {
+    ProgressBarSection,
+    FormSection,
+  },
+};
+</script>
