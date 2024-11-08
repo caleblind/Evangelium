f99dcfa21340760ebe6d662538c4c7390c39479e
Jez Abot <jez.abot@students.pcci.edu>
2024-11-08 15:06:50 -0600

    Saved serializer changes because pylint was complaining

--- Backend/BaseApp/serializer.py
+++ Backend/BaseApp/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+#from django.contrib.auth import authenticate
 from .models import User, Supporter, Missionary,\
                     Tag, TagRecord, SearchHistory, ExternalMedia
 
