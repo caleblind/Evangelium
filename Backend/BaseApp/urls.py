87a814836f4124a24a7d3feacbb315397930b81d
Jez Abot <jez.abot@students.pcci.edu>
2024-11-08 01:21:34 -0600

    Added basic viewset to URLs (not including account creation, login, or logout)

--- Backend/BaseApp/urls.py
+++ Backend/BaseApp/urls.py
@@ -6,7 +6,13 @@ from . import views
 
 #Automatically generates URLs for all ViewSet classes
 router = routers.DefaultRouter()
+router.register('missionary', views.UserViewSet)
+router.register('missionary', views.SupporterViewSet)
 router.register('missionary', views.MissionaryViewSet)
+router.register('missionary', views.TagViewSet)
+router.register('missionary', views.TagRecordViewSet)
+router.register('missionary', views.SearchHistoryViewSet)
+router.register('missionary', views.ExternalMediaViewSet)
 
 urlpatterns = [
    path('', include(router.urls))
