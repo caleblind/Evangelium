c4c70525ce9014c621cb72e28d130d77dd0a7f84
Jez Abot <jez.abot@students.pcci.edu>
2024-11-07 21:25:32 -0600

    Pylint stuff in models.py

--- Backend/BaseApp/models.py
+++ Backend/BaseApp/models.py
@@ -32,8 +32,11 @@ class Church(models.Model):
    def __str__(self):
       return str(self.church_name)
 
-#Manager for custom user models
+#Custom djnago user manager for custom django user model
 class UserManager(BaseUserManager):
+
+   #Handles user creation by ensuring email entry/normalization
+   #and password hashing/normalization
    def create_user(self, email, password=None, **extra_fields):
       if not email:
          raise ValueError("The Email field must be set")
@@ -42,3 +45,27 @@ class UserManager(BaseUserManager):
       user.set_password(password)
       user.save(using=self._db)
       return user
+
+   #Handles superuser creation for admins (django admin page users)
+   def create_superuser(self, email, password=None, **extra_fields):
+      extra_fields.setdefault('is_staff', True)
+      extra_fields.setdefault('is_super', True)
+      if extra_fields.get("is_staff") is not True:
+         raise ValueError("Superuser must have is_staff=True.")
+      if extra_fields.get("is_superuser") is not True:
+         raise ValueError("Superuser must have is_superuser=True.")
+
+      return self.create_user(email, password, **extra_fields)
+
+#Custom django user model
+class User(AbstractBaseUser):
+   email = models.EmailField(max_length=254, unique=True, null=False)
+   password = models.CharField(max_length=128)
+   user_type = models.CharField(max_length=15)
+   profile_picture = models.URLField(max_length=225, null=True, blank=True)
+   description = models.TextField(null=True, blank=True)
+   phone_number = models.CharField(max_length=100, null=False)
+
+   #Links the custom django user manager to this custom user
+   objects = UserManager()
+   USERNAME_FIELD = 'email'
