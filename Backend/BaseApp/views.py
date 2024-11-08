a3cfae9f5901ebfa5b91cdfa7249f696244bb872
Jez Abot <jez.abot@students.pcci.edu>
2024-11-08 00:51:03 -0600

    Deleted old views and added new api views (not including login/account creation views)

--- Backend/BaseApp/views.py
+++ Backend/BaseApp/views.py
@@ -1,79 +1,54 @@
-# Imports
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import status
-from rest_framework.decorators import api_view
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
-from .models import Missionary
-from .serializer import MissionarySerializer
+#from rest_framework.decorators import api_view, permission_classes, authentication_classes
+#from django.contrib.auth import login, logout
+#from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import User, Supporter, Missionary,\
+                    Tag, TagRecord, SearchHistory,\
+                    ExternalMedia
+from .serializer import UserSerializer, SupporterSerializer,\
+                        MissionarySerializer, TagSerializer,\
+                        TagRecordSerializer, SeachHistorySerializer,\
+                        ExternalMediaSerializer
 
-# Gets all church and missionary objects and renders the data on matching.html
-#@login_required
-#def matching(request):
-#   church_data = Church.objects.all()
-#  missionary_data = Missionary.objects.all()
+# API viewset for Users that performs CRUD operations
+class UserViewSet(ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [IsAuthenticated]
 
-#  data = {
-#       'churches': church_data,
-#       'missionaries': missionary_data,
-#   }
-#   return render(request, 'matching.html', context=data)
+# API viewset for Supporters that performs CRUD operations
+class SupporterViewSet(ModelViewSet):
+   queryset = Supporter.objects.all()
+   serializer_class = SupporterSerializer
+   permission_classes = [IsAuthenticated]
 
-@login_required
-def home(request):
-   return render(request,"matching.html")
-
-def authView(request):
-   if request.method =="POST":
-      form = UserCreationForm(request.POST or None)
-      if form.is_valid():
-         form.save()
-         return redirect("/accounts/login/")
-   else:
-      form = UserCreationForm()
-   return render(request, "registration/signup.html",{"form": form})
-
-#Basic ViewSet class for churches that can perform all CRUD operations
-#class ChurchViewSet(ModelViewSet):
-#   queryset = Church.objects.all()
-#   serializer_class = ChurchSerializer
-
-#Basic ViewSet class for missionaries that can perform all CRUD operations
+# API viewset for Missionaries that performs CRUD operations
 class MissionaryViewSet(ModelViewSet):
    queryset = Missionary.objects.all()
    serializer_class = MissionarySerializer
+   permission_classes = [IsAuthenticated]
 
-#Mock endpoint that the account creation page uses for reference
-@api_view(['POST'])
-def account_creation(request):
-   if request.method =="POST":
-      return JsonResponse({"message": "Request recieved!"},
-                          status=status.HTTP_200_OK)
-   return None
+# API viewset for Tags that performs CRUD operations
+class TagViewSet(ModelViewSet):
+   queryset = Tag.objects.all()
+   serializer_class = TagSerializer
+   permission_classes = [IsAuthenticated]
 
-#Mock endpoint that the login page can use for reference
-@api_view(['POST', 'GET'])
-def user_login(request):
-   if request.method == "POST":
-      return JsonResponse({"message": "Request recieved!"},
-                          status=status.HTTP_200_OK)
-   if request.method == "GET":
-      return JsonResponse({"message": "Request recieved!"},
-                          status=status.HTTP_200_OK)
-   return None
+# API viewset for Tag Records that performs CRUD operations
+class TagRecordViewSet(ModelViewSet):
+   queryset = TagRecord.objects.all()
+   serializer_class = TagRecordSerializer
+   permission_classes = [IsAuthenticated]
 
-#Mock endpoint that the Profile Page can use for reference
-@api_view(['PUT', 'GET', 'POST'])
-def user_profile(request):
-   if request.method == 'PUT':
-      return JsonResponse({"message": "Request recieved!"},
-                          status=status.HTTP_200_OK)
-   if request.method == 'POST':
-      return JsonResponse({"message": "Request recieved!"},
-                          status=status.HTTP_200_OK)
-   if request.method == 'GET':
-      return JsonResponse({"message": "Request recieved!"},
-                          status=status.HTTP_200_OK)
-   return None
+# API viewset for Search History that performs CRUD operations
+class SearchHistoryViewSet(ModelViewSet):
+   queryset = SearchHistory.objects.all()
+   serializer_class = SeachHistorySerializer
+   permission_classes = [IsAuthenticated]
+
+# API viewset for External Media that performs CRUD operations
+class ExternalMediaViewSet(ModelViewSet):
+   queryset = ExternalMedia.objects.all()
+   serializer_class = ExternalMediaSerializer
+   permission_classes = [IsAuthenticated]
