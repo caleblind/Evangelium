c3f3ffed4bf367acf45020ce58d571340d8f9662
Jez Abot <jez.abot@students.pcci.edu>
2024-11-11 19:36:35 -0600

    Added serializer and view for user login (no auth)

--- Backend/BaseApp/urls.py
+++ Backend/BaseApp/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path, include
 from rest_framework import routers
-from . import views
+from .views import LoginAPIView, UserViewSet, SupporterViewSet,\
+                   MissionaryViewSet, TagViewSet, TagRecordViewSet,\
+                   SearchHistoryViewSet, ExternalMediaViewSet
 
 #Automatically generates URLs for all ViewSet classes
 router = routers.DefaultRouter()
-router.register('user', views.UserViewSet)
-router.register('supporter', views.SupporterViewSet)
-router.register('missionary', views.MissionaryViewSet)
-router.register('tag', views.TagViewSet)
-router.register('tagrecord', views.TagRecordViewSet)
-router.register('searchhistory', views.SearchHistoryViewSet)
-router.register('externalmedia', views.ExternalMediaViewSet)
+router.register('user', UserViewSet)
+router.register('supporter', SupporterViewSet)
+router.register('missionary', MissionaryViewSet)
+router.register('tag', TagViewSet)
+router.register('tagrecord', TagRecordViewSet)
+router.register('searchhistory', SearchHistoryViewSet)
+router.register('externalmedia', ExternalMediaViewSet)
 
 urlpatterns = [
-   path('', include(router.urls))
+   path('', include(router.urls)),
+   path('login/', LoginAPIView.as_view(), name="login"),
 ]
--- Backend/BaseApp/views.py
+++ Backend/BaseApp/views.py
@@ -1,16 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
 #from rest_framework.decorators import api_view, permission_classes,\
 #                                      authentication_classes
-#from django.contrib.auth import login, logout
+from django.contrib.auth import login  #logout
 #from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
+#from django.views.decorators.csrf import ensure_csrf_cookie
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import User, Supporter, Missionary,\
                     Tag, TagRecord, SearchHistory,\
                     ExternalMedia
 from .serializer import UserSerializer, SupporterSerializer,\
                         MissionarySerializer, TagSerializer,\
                         TagRecordSerializer, SeachHistorySerializer,\
-                        ExternalMediaSerializer
+                        ExternalMediaSerializer, LoginSerializer
 
 # API viewset for Users that performs CRUD operations
 class UserViewSet(ModelViewSet):
@@ -53,3 +57,18 @@ class ExternalMediaViewSet(ModelViewSet):
    queryset = ExternalMedia.objects.all()
    serializer_class = ExternalMediaSerializer
    permission_classes = [IsAuthenticated]
+
+
+# API view for validating user login
+class LoginAPIView(APIView):
+   serializer_class = LoginSerializer
+   permission_classes = [AllowAny]
+
+   # Logs user in and creates a session
+   def post(self, request):
+      serializer = LoginSerializer(data=request.data)
+      if serializer.is_valid():
+         user = serializer.validated_data
+         login(request, user)
+         return Response(serializer.data, status=status.HTTP_200_OK)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
