9fdcaad60bbe289a6608195022f6f9e8d2400e26
Kraken <cekahnke@gmail.com>
2025-02-20 21:03:15 -0600

    Added get current user funcitonality to backend with no other changes

--- Backend/BaseApp/urls.py
+++ Backend/BaseApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import TagViewSet, SearchHistoryViewSet,\
                    ExternalMediaViewSet,\
-                   ProfileListCreateView, ProfileDetailView
+                   ProfileListCreateView, ProfileDetailView, CurrentUserProfileView
 
 #Automatically generates URLs for all ViewSet classes
 router = routers.DefaultRouter()
@@ -14,6 +14,8 @@ urlpatterns = [
    path('', include(router.urls)),
    path('api/profiles/', ProfileListCreateView.as_view(),
         name='profile-list-create'),
+   path('api/profiles/me/', CurrentUserProfileView.as_view(),
+        name='current-user-profile'),  # Uses same view
    path('api/profiles/<int:pk>/', ProfileDetailView.as_view(),
         name='profile-detail'),
 ]
