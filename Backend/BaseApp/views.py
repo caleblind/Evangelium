89c5bf14399388764aa322ac97540653bed52e8f
caleblind <57119431+caleblind@users.noreply.github.com>
2025-02-22 21:20:45 -0500

    Updates for matchmaking algorithm

--- Backend/BaseApp/views.py
+++ Backend/BaseApp/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import generics, filters
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.db.models import Q
 from .models import Tag, SearchHistory,\
                     ExternalMedia, Profile
 from .serializer import TagSerializer, SeachHistorySerializer,\
@@ -24,22 +26,27 @@ class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 class MatchmakingResultsView(generics.ListAPIView):
    serializer_class = ProfileSerializer
+   authentication_classes = [JWTAuthentication]
    permission_classes = [IsAuthenticated]  # Only authenticated users can access
 
    def get_queryset(self):
-      user = self.request.user
+      user_profile = Profile.objects.filter(
+         user=self.request.user).prefetch_related('tags').first()
 
-      # Ensure the current user has a profile
-      if not hasattr(user, 'profile'):
+      if not user_profile:
          return Profile.objects.none()
+         # Return an empty queryset if the user has no profile
 
-        # Get the current user's tags
-      user_tags = user.profile.tags.all()
+      # Get the tags associated with the current user's profile
+      user_tags = user_profile.tags.all()
 
-        # Filter profiles that share any tag with the current user,
-        # excluding the current user's own profile, and ensure uniqueness.
-      return Profile.objects.filter(tags__in=user_tags).exclude(
-         user=user).distinct()
+      # Find profiles with at least one matching tag, excluding
+      # the current user's profile
+      matching_profiles = Profile.objects.filter(
+         Q(tags__in=user_tags)).exclude(
+            user=self.request.user).distinct()
+
+      return matching_profiles
 
 # Tag viewset that performs CRUD operations
 class TagViewSet(ModelViewSet):
