ebfddcf8fbbc645e7ec5f8d7c13740f2f42ed684
Jez Abot <jez.abot@students.pcci.edu>
2025-02-26 21:01:52 -0600

    Added more forms to be visible as pages

--- Frontend/src/router/router.js
+++ Frontend/src/router/router.js
@@ -4,6 +4,8 @@ import UserProfile from "@/pages/UserProfile.vue";
 import LandingPage from "@/pages/LandingPage.vue";
 import AccountCreation from "@/pages/AccountCreation.vue";
 import MissionaryForm from "@/components/accountcreation/MissionaryForm.vue";
+import MainForm from "@/components/accountcreation/MainForm.vue";
+import DonorForm from "@/components/accountcreation/DonorForm.vue";
 
 const routes = [
   { path: "/", component: LandingPage },
@@ -12,6 +14,8 @@ const routes = [
   { path: "/LandingPage", component: LandingPage }, // duplicate. remove when sure it won't break anything
   { path: "/AccountCreation", component: AccountCreation },
   { path: "/MissionaryForm", component: MissionaryForm },
+  { path: "/MainForm", component: MainForm },
+  { path: "/DonorForm", component: DonorForm },
 ];
 
 const router = createRouter({
