4a39e53d37c8b4bd7bb9089b6895cff79804240b
caleblind <57119431+caleblind@users.noreply.github.com>
2025-03-13 13:17:22 -0400

    Update settings to fix env issue

--- Backend/saltnlight/settings.py
+++ Backend/saltnlight/settings.py
@@ -13,12 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,7 +122,8 @@ WSGI_APPLICATION = 'saltnlight.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-   'default': dj_database_url.config(env='DATABASE_URL', conn_max_age=600, ssl_require=True)
+   'default': dj_database_url.config(default=os.getenv("DATABASE_URL"),
+                                      conn_max_age=600, ssl_require=True)
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
