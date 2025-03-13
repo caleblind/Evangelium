9701d843dfe8bc85ea52f40a24b7e2f2184fecc5
Jez Abot <jez.abot@students.pcci.edu>
2025-03-12 22:01:11 -0500

    switching back to sqlite for developement

--- Backend/saltnlight/settings.py
+++ Backend/saltnlight/settings.py
@@ -125,11 +125,11 @@ WSGI_APPLICATION = 'saltnlight.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-   'default': dj_database_url.config(env='DATABASE_URL', conn_max_age=600, ssl_require=True)
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
+#   'default': dj_database_url.config(env='DATABASE_URL', conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 #if os.getenv('USE_POSTGRES'):
